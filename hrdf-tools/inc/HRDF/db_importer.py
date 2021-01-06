a457003969b12d5f853a6ec5387c7025d7c13271
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-01-06 23:59:04 +0100

    Adds BFKOORD_WGS parser

--- hrdf-tools/inc/HRDF/db_importer.py
+++ hrdf-tools/inc/HRDF/db_importer.py
@@ -8,6 +8,7 @@ from .HRDF_Parser.parse_bitfeld import import_db_bitfeld
 from .HRDF_Parser.parse_fplan_stop_times import import_db_stop_times
 from .HRDF_Parser.parse_fplan import import_db_fplan
 from .HRDF_Parser.parse_gleis import import_db_gleis
+from .HRDF_Parser.parse_stops import import_db_stops
 
 class HRDF_DB_Importer:
     def __init__(self, hrdf_path, db_path):
@@ -23,6 +24,7 @@ class HRDF_DB_Importer:
         import_db_fplan(self.hrdf_path, self.db_path, self.db_schema_config)
         import_db_stop_times(self.hrdf_path, self.db_path, self.db_schema_config)
         import_db_betrieb(self.hrdf_path, self.db_path, self.db_schema_config)
+        import_db_stops(self.hrdf_path, self.db_path, self.db_schema_config)
 
         log_message("DONE")
 

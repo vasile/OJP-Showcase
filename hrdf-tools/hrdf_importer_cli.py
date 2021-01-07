834ffc0d493a8f80ae1ec2a799efecb62509b478
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2021-01-07 09:33:54 +0100

    Semantics: Fixes DB custom path parameter

--- hrdf-tools/hrdf_importer_cli.py
+++ hrdf-tools/hrdf_importer_cli.py
@@ -7,7 +7,7 @@ from inc.HRDF.HRDF_Parser.hrdf_helpers import compute_formatted_date_from_hrdf_f
 
 parser = argparse.ArgumentParser(description = 'Import HRDF folder into a SQLite DB')
 parser.add_argument('-p', '--path', help='Path to HRDF folder')
-parser.add_argument('-o', '--output', help='SQLite output filename')
+parser.add_argument('-o', '--output', help='SQLite output path')
 
 args = parser.parse_args()
 input_path = args.path
@@ -18,22 +18,21 @@ if input_path is None:
 
 hrdf_path = os.path.abspath(input_path)
 
-db_filename = None
-if args.output:
-    db_filename = args.output
-else:
-    formatted_date = compute_formatted_date_from_hrdf_folder_path(hrdf_path)
-    
-    if formatted_date is None:
-        print(f"CANT read date from HRDF path: '{hrdf_path}'")
-        print(f"Use --filename to override")
-        sys.exit(1)
-    
-    db_filename = f"hrdf_{formatted_date}.sqlite"
+formatted_date = compute_formatted_date_from_hrdf_folder_path(hrdf_path)  
+if formatted_date is None:
+    print(f"CANT read date from HRDF path: '{hrdf_path}'")
+    print(f"Use --filename to override")
+    sys.exit(1)
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-db_path = f"{dir_path}/tmp/{db_filename}"
-db_path = os.path.abspath(db_path)
+db_filename = f"hrdf_{formatted_date}.sqlite"
+
+db_path = None
+if args.output:
+    db_path = args.output
+else:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    db_path = f"{dir_path}/tmp/{db_filename}"
+    db_path = os.path.abspath(db_path)    
 
 print(f"IMPORT to: {db_path}")
 
